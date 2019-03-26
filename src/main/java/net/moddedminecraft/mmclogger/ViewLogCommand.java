d54802cc2bab8097d4fa7ceafad24ff4caaa1477
leelawd <leelawd123@gmail.com>
2019-03-26 18:12:43 +0000

    Update ViewLogCommand.java

--- src/main/java/net/moddedminecraft/mmclogger/ViewLogCommand.java
+++ src/main/java/net/moddedminecraft/mmclogger/ViewLogCommand.java
@@ -16,8 +16,10 @@ import org.spongepowered.api.text.action.TextActions;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -38,18 +40,8 @@ public class ViewLogCommand implements CommandExecutor {
         File[] files = plugin.rootFolder.listFiles();
 
         if (playerOp.isPresent()) {
-            try {
-                Text.Builder send = Text.builder();
-                String url = "http://hastebin.com/" + getHaste(new File(plugin.playersFolder + "/" + playerOp.get().getName() + ".log"));
-                send.append(plugin.fromLegacy("&3" + url));
-                send.onClick(TextActions.openUrl(new URL(url)));
-                send.onHover(TextActions.showText(plugin.fromLegacy("&eClick here to go to: &6" + url)));
-                src.sendMessage(send.build());
-                return CommandResult.success();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return CommandResult.empty();
-            }
+            getHaste(new File(plugin.playersFolder + "/" + playerOp.get().getName() + ".log"), src);
+            return CommandResult.success();
         } else {
 
             for (File file : files) {
@@ -80,69 +72,88 @@ public class ViewLogCommand implements CommandExecutor {
         return consumer -> {
             PaginationService paginationService = Sponge.getServiceManager().provide(PaginationService.class).get();
             java.util.List<Text> contents = new ArrayList<>();
-
-            for (File file : folder.listFiles()) {
-                Text.Builder send = Text.builder();
-                if (file.isDirectory()) {
-                    send.append(plugin.fromLegacy("&8[&e" + file.getName() + "&8]"));
-                    send.onClick(TextActions.executeCallback(viewDir(file)));
-                    send.onHover(TextActions.showText(plugin.fromLegacy("&eClick to view folder")));
-                } else {
-                    send.append(plugin.fromLegacy("&e" + file.getName()));
-                    send.onClick(TextActions.executeCallback(viewFile(file)));
-                    send.onHover(TextActions.showText(plugin.fromLegacy("&eClick to get a hastebin link for this file")));
+            Sponge.getScheduler().createTaskBuilder().execute(() -> {
+                for (File file : folder.listFiles()) {
+                    Text.Builder send = Text.builder();
+                    if (file.isDirectory()) {
+                        send.append(plugin.fromLegacy("&8[&e" + file.getName() + "&8]"));
+                        send.onClick(TextActions.executeCallback(viewDir(file)));
+                        send.onHover(TextActions.showText(plugin.fromLegacy("&eClick to view folder")));
+                    } else {
+                        send.append(plugin.fromLegacy("&e" + file.getName()));
+                        send.onClick(TextActions.executeCallback(viewFile(file)));
+                        send.onHover(TextActions.showText(plugin.fromLegacy("&eClick to get a hastebin link for this file")));
+                    }
+                    contents.add(send.build());
                 }
-                contents.add(send.build());
-            }
 
-            paginationService.builder()
-                    .title(plugin.fromLegacy("&8[&7" + folder.getName() + "&8]"))
-                    .contents(contents)
-                    .padding(plugin.fromLegacy("&8="))
-                    .sendTo(consumer);
+                paginationService.builder()
+                        .title(plugin.fromLegacy("&8[&7" + folder.getName() + "&8]"))
+                        .contents(contents)
+                        .padding(plugin.fromLegacy("&8="))
+                        .sendTo(consumer);
+            }).name("mmclogger-async-viewdir").async().submit(plugin);
         };
     }
 
     private Consumer<CommandSource> viewFile(File file) {
         return consumer -> {
-            try {
-                Text.Builder send = Text.builder();
-                String url = "http://hastebin.com/" + getHaste(file);
-                send.append(plugin.fromLegacy("&3" + url));
-                send.onClick(TextActions.openUrl(new URL(url)));
-                send.onHover(TextActions.showText(plugin.fromLegacy("&eClick here to go to: &6" + url)));
-                consumer.sendMessage(send.build());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            getHaste(file, consumer);
         };
     }
 
-    public String getHaste(File file) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL("https://hastebin.com/documents").openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-        connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+    public void getHaste(File file, CommandSource src) {
+        Sponge.getScheduler().createTaskBuilder().execute(() -> {
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) new URL("https://hastebin.com/documents").openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+                
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                List<String> fileList = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+                StringBuilder sb = new StringBuilder();
+                int numLines = fileList.size();
+                if (numLines > 6000) {
+                    numLines = 6000;
+                }
+                List<String> lastLines = fileList.subList(fileList.size()-numLines, fileList.size());
+                for (String line : lastLines) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+                wr.writeBytes(sb.toString());
+                wr.flush();
+                wr.close();
 
-        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-        wr.writeBytes(new String(Files.readAllBytes(file.toPath())));
-        wr.flush();
-        wr.close();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
 
-        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
+                while ((inputLine = rd.readLine()) != null) response.append(inputLine);
+                rd.close();
 
-        while ((inputLine = rd.readLine()) != null) response.append(inputLine);
-        rd.close();
+                JsonElement json = new JsonParser().parse(response.toString());
+                if (!json.isJsonObject()) {
+                    throw new IOException("Can't parse JSON");
+                }
 
-        JsonElement json = new JsonParser().parse(response.toString());
-        if (!json.isJsonObject()) {
-            throw new IOException("Can't parse JSON");
-        }
-
-        return json.getAsJsonObject().get("key").getAsString();
+                Text.Builder send = Text.builder();
+                String url = "http://hastebin.com/" + json.getAsJsonObject().get("key").getAsString();
+                send.append(plugin.fromLegacy("&3" + url));
+                send.onClick(TextActions.openUrl(new URL(url)));
+                send.onHover(TextActions.showText(plugin.fromLegacy("&eClick here to go to: &6" + url)));
+                src.sendMessage(send.build());
+            } catch (IOException e) {
+                e.printStackTrace();
+                src.sendMessage(plugin.fromLegacy("&cThere was an error getting your hastebin"));
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }).name("mmclogger-async-gethaste").async().submit(plugin);
     }
 }
